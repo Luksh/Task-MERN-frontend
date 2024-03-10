6876ece2a8a5093e67b9c6f93e51771ac2eae07b
Laxman Maharjan <hotmale_luksh@hotmail.com>
2024-03-10 08:06:18 +0545

    Register Form

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,9 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaUser } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { register, reset } from "../features/auth/authSlice";
+import Spinner from "./Spinner";
 
 const Register = () => {
   const [formData, setFormData] = useState({ name: "", email: "", password: "", password2: "" });
   const { name, email, password, password2 } = formData;
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user, isLoading, isError, isSuccess, message } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (isError) toast.error(message);
+    if (isSuccess || user) navigate("/");
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -14,13 +29,22 @@ const Register = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
+    if (password !== password2) {
+      toast.error("Passwords are different");
+    } else {
+      const userData = { name, email, password };
+      dispatch(register(userData));
+    }
   };
 
-  return (
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <>
       <section className="heading">
         <h1>
-          <FaUser /> Register
+          <FaUser />
+          Register
         </h1>
         <p>Please create an account</p>
       </section>
