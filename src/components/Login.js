1432cdfe5c00d88edb43de97c02df18247055f9c
Laxman Maharjan <hotmale_luksh@hotmail.com>
2024-03-23 07:52:11 +0545

    Login Form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,25 @@
 import { FaSignInAlt } from "react-icons/fa";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { login, reset } from "../features/auth/authSlice";
+import Spinner from "./Spinner";
 
 const Login = () => {
   const [formData, setFormData] = useState({ email: "", password: "" });
   const { email, password } = formData;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (isError) toast.error(message);
+
+    if (isSuccess || user) navigate("/");
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -14,8 +30,12 @@ const Login = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
+    const userData = { email, password };
+    dispatch(login(userData));
   };
-  return (
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <>
       <section className="heading">
         <h1>
